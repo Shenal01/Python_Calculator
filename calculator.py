c70daf0ae8d398810a1e0bc12d7ea116466f7646
Shenal Mario <125433645+Shenal01@users.noreply.github.com>
2023-12-06 12:11:14 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,4 +1,5 @@
 import math
+history = []
 
 while True:
     print('Select operation.')
@@ -10,8 +11,9 @@ while True:
     print("6.Remainder: %")
     print("7.Terminate: #")
     print("8.Reset    : $")
-
-    operation = input('Enter choice(+,-,*,/,^,%,#,$): ')
+    print("8.History  : ?")
+    
+    operation = input('Enter choice(+,-,*,/,^,%,#,$,?): ')
     print(operation)
 
     if operation == '#':
@@ -22,50 +24,67 @@ while True:
         print("Reseting... ")
         continue
 
+    elif operation == '?':
+            if history:
+                for last_calculation in history:
+                    print(last_calculation)
+                continue
+            else:
+                print('No past calculations to show')
+                continue
+
     num1 = None
     num2 = None
+    result = 0.0
+    last_calculation = ""
 
     try:
         num1 = str(input('Enter first number: '))
-        print(int(num1))
+        print(str(num1))
         num2 = str(input('Enter second number: '))
-        print(int(num2))
+        print(str(num2))
         num1 = float(num1)
         num2 = float(num2)
 
         if operation == '+':
             result = num1 + num2
-            print(f'{num1} {operation} {num2} = {result}')
 
         elif operation == '-':
             result = num1 - num2
-            print(f'{num1} {operation} {num2} = {result}')
 
         elif operation == '*':
             result = num1 * num2
-            print(f'{num1} {operation} {num2} = {result}')
-
+        
         elif operation == '/':    
             if float(num1) is not None and float(num2) is not None:
                         if num2 != 0:
                             result = num1 / num2
-                            print(f"{num1} / {num2} = {result}")
+
                         else:
                             print("float division by zero")
-                            print(f"{num1} / {num2} = None")
+                            last_calculation =  "{0} {1} {2} = {3}".format(num1, operation, num2, None) 
+                            print(last_calculation )
+                            history.append(last_calculation)
                             num2 = str(num2)
                             continue
         
         elif operation == '^':
             result = math.pow(num1, num2)
-            formated_result = "{:.3f}".format(result)
-            print(f'{num1} {operation} {num2} = {formated_result}')
+            #formated_result = "{:.3f}".format(result)
+            #print(f'{num1} {operation} {num2} = {formated_result}')
 
         elif operation == '%':
             result = num1 % num2
-            print(f'{num1} {operation} {num2} = {result}')
 
-        
+        else:
+            print('Invalid operation')
+            exit()
+
+        last_calculation =  "{0} {1} {2} = {3}".format(num1, operation, num2, result) 
+        print(last_calculation )
+        history.append(last_calculation)
+
+    
 
     except ValueError as e:
                 #data1 = type(num1)
@@ -95,4 +114,3 @@ while True:
                     num1 = str(num1)
                     print(f"{num1}")
                     continue
-
